82e605c48afc7fe94389cb985d82c9c17963b9e6
Murtadha Alobaidi <mhaao@kth.se>
2023-01-08 17:06:50 +0100

    Project hosting done
    hosting in firebase done
    
    Co-Authored-By: Abdullah Trabulsiah <83720452+Abdullahtr249@users.noreply.github.com>

--- boka-kth/src/components/InlineTextButton.js
+++ boka-kth/src/components/InlineTextButton.js
@@ -1,23 +1,17 @@
-import { Text, Pressable } from 'react-native'
-import AppStyles from '../styles/AppStyles'
+import { Text, Pressable } from 'react-native';
+import AppStyles from '../styles/AppStyles';
 
 export default function InlineTextButton(props) {
-  let style = {}
+  let style = {};
   if (props.color) {
     style.color = props.color
-  }
+  };
   return (
     <Pressable onPress={props.onPress}>
       {({ pressed }) => (
-        <Text
-          style={[
-            pressed
-              ? AppStyles.pressedInlineTextButton
-              : AppStyles.inlineTextButton,
-            style,
-          ]}
-        >
-          {props.text}
+        <Text 
+          style={[pressed ? AppStyles.pressedInlineTextButton : AppStyles.inlineTextButton, style]}>
+            {props.text}
         </Text>
       )}
     </Pressable>
